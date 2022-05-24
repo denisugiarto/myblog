f2ecf9cc3fdb2a6d890d5b04ccdaec519fd26e20
Deni Sugiarto <sugiarto.deni@gmail.com>
2022-05-24 16:22:20 +0700

    fix upload cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,13 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64,
+      },
       provider: "cloudinary",
       providerOptions: {
         cloud_name: env("CLOUDINARY_NAME"),
